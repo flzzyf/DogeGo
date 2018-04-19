c89e0e34e6bf826071901a742e36dd51534a5157
flzzyf <2981019122@qq.com>
2018-04-19 19:07:12 +0800

    Init

---
+++ DogeGo/Assets/GyroCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCamera : MonoBehaviour {
+
+    Gyroscope gyroscope;
+    bool gyroSupported;
+
+	void Start () 
+    {
+        gyroSupported = SystemInfo.supportsGyroscope;
+
+        if(gyroSupported)
+        {
+            print("Gyro Supported!");
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+        }
+	}
+	
+	void Update () 
+    {
+        transform.rotation = gyroscope.attitude;
+	}
+}

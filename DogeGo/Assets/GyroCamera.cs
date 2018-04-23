1056f3f883c745422a9c977f536351239191307a
Flzzyf <flzzyf076@gmail.com>
2018-04-23 21:36:00 +0800

    qwe

--- DogeGo/Assets/GyroCamera.cs
+++ DogeGo/Assets/GyroCamera.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GyroCamera : MonoBehaviour 
 {
     public Transform worldObj;
+
     float startY;
     Quaternion rotationFix;
 
     Gyroscope gyroscope;
     bool gyroSupported;
 
+    public Text[] t;
+
 	void Start () 
     {
         gyroSupported = SystemInfo.supportsGyroscope;
+        t[0].text = "Gyro: " + gyroSupported.ToString();
 
         GameObject camParent = new GameObject("CamParent");
+        camParent.transform.position = transform.position;
+        transform.parent = camParent.transform;
 
         if(gyroSupported)
         {
@@ -27,7 +34,9 @@ public class GyroCamera : MonoBehaviour
             rotationFix = new Quaternion(0, 0, 1, 0);
         }
 	}
-	
+
+    int a = 0;
+
 	void Update () 
     {
         if(gyroSupported && startY == 0)
@@ -36,8 +45,23 @@ public class GyroCamera : MonoBehaviour
         }
 
         if (gyroSupported)
-            transform.rotation = gyroscope.attitude * rotationFix;
-	}
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+
+            //transform.rotation = gyroscope.attitude * rotationFix;
+            transform.rotation = gyroscope.attitude;
+
+            a++;
+
+            //t[1].text = "Att: " + Input.gyro.attitude;
+            t[1].text = transform.rotation.ToString();
+            t[2].text = a.ToString();
+            //t[2].text = "Att: " + Input.gyro.attitude.eulerAngles;
+        }
+
+
+    }
 
     void ResetGyroRotation()
     {
---
+++ DogeGo/Assets/buck.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buck : MonoBehaviour {
+    bool draw = false;
+    bool gyinfo;
+    Gyroscope go;
+    void Start()
+    {
+        gyinfo = SystemInfo.supportsGyroscope;
+        go = Input.gyro;
+        go.enabled = true;
+    }
+    void Update()
+    {
+
+
+        if (buttoned)
+        {
+            t[0].text = go.attitude.eulerAngles.ToString();
+            if (gyinfo)
+            {
+                Vector3 a = go.attitude.eulerAngles;
+                a = new Vector3(-a.x, -a.y, a.z); //直接使用读取的欧拉角发现不对，于是自己调整一下符号  
+                this.transform.eulerAngles = a;
+                this.transform.Rotate(Vector3.right * 90, Space.World);
+                draw = false;
+            }
+            else
+            {
+                draw = true;
+            }
+        }
+    }
+    public Text[] t;
+
+    void OnGUI()
+    {
+        if (draw)
+        {
+            GUI.Label(new Rect(100, 100, 100, 30), "启动失败");
+        }
+    }
+    bool buttoned = false;
+    public void button()
+    {
+        buttoned = true;
+    }
+}

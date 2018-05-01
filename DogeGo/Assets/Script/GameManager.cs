d2fc3965ad07a7345ea22ab5e1361d0bf255dc4b
flzzyf <2981019122@qq.com>
2018-05-01 12:42:46 +0800

    导入AR插件，加上Menu

--- DogeGo/Assets/Script/GameManager.cs
+++ DogeGo/Assets/Script/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour 
 {
@@ -9,9 +10,17 @@ public class GameManager : MonoBehaviour
 
 	private void Awake()
 	{
-        instance = this;
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
 	}
 
 	public Text[] t;
+
+    public void LoadScene(int _index)
+    {
+        SceneManager.LoadScene(_index);
+    }
 
 }

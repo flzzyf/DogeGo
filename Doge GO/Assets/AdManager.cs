17ca7a0a10e8a5fdd1d13a1e587a496cb5a7cb76
Flzzyf <flzzyf076@gmail.com>
2018-05-06 11:27:19 +0800

    广告加入

---
+++ Doge GO/Assets/AdManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            ShowAd("reward");
+        }
+	}
+
+    public void ShowAd(string _id)
+    {
+        if (Advertisement.IsReady(_id))
+        {
+            Advertisement.Show(_id);
+        }
+    }
+}

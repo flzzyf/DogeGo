7cf5304bcc64f2d131f027f8d7ad9e03da527c28
flzzyf <2981019122@qq.com>
2018-07-08 16:23:40 +0800

    AR基本完成

---
+++ Doge GO/Assets/Script/AR/VuforiaManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Vuforia;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class HitTestResultEvent : UnityEvent<HitTestResult> { }
+
+public class VuforiaManager : MonoBehaviour
+{
+    public HitTestResultEvent clickEvent;
+
+    public bool canTrigger = false;
+
+    public void OnInteractiveHitTest(HitTestResult result)
+    {
+        if (canTrigger)
+            clickEvent.Invoke(result);
+    }
+
+    [HideInInspector]
+    public bool hit = false;
+
+    public void Hit()
+    {
+        hit = true;
+    }
+
+    private void LateUpdate()
+    {
+        hit = false;
+    }
+}
